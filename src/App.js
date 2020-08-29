17ea8b110b390e915fff1817c3d8dea05bb34359
digbijay1 <digbijay.das@suiit.ac.in>
2020-08-28 23:43:25 -0700

    code pushed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import  React, { useState } from 'react'
+
+const App = () => {
+  const [count,setCount]= useState(0);
+
+const IncNum= ()=>
+{
+  setCount(count+1)
+}
+const Dec=()=>
+{
+  setCount(count-1)
+}
+return (
+  <>
+  <h1> {count} </h1>
+  <button onClick={IncNum}> click me</button>
+  <button onClick={Dec}> click me</button>
+
+  </>
+)
+}
+export default App
